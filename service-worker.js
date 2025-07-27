b166ba06bd4e5d71f40f3f35e5dceec715f3856e
fransilvafsd <franciscoraw379@gmail.com>
2025-07-27 05:10:53 +0000

    Add files via upload
    Signed-off-by: fransilvafsd <franciscoraw379@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,21 @@
-const CACHE_NAME = 'fsd-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/premium.html',
-  '/premium-sucesso.html',
-  '/style.css',
-  '/app.js',
-  '/icon.png',
-  '/manifest.webmanifest'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open("fsd-cache-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
